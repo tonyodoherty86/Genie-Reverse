3b6674ae7a08c9c628123030240f34e0641c2ce4
Yvan Burrie <yvan.burrie@hotmail.com>
2018-07-01 01:44:16 +1000

    Add RGE and TRB module headers

---
+++ src/Engine.hpp
@@ -0,0 +1,61 @@
+
+/**
+ * @file    Engine.hpp
+ * @author  Yvan Burrie
+ * @date    2018/06/28
+ * @version 1.0
+ */
+
+#define ENGINE_ROR 1
+#define ENGINE_AOK 1
+#define ENGINE_AOC 1
+
+/* Include essential components: */
+#include "Engine\ResFile.h"
+#include "Engine\DIB.h"
+
+/* Include graphical components: */
+#include "Engine\SpanList.hpp"
+#include "Engine\DrawSystem.hpp"
+#include "Engine\DrawArea.hpp"
+#include "Engine\AsmDraw.h"
+#include "Engine\Shape.hpp"
+#include "Engine\MousePointer.hpp"
+
+/* Include audible components: */
+#include "Engine\SoundDriver.hpp"
+#include "Engine\Digital.hpp"
+#include "Engine\Music.hpp"
+
+/* Include communication components: */
+#define PLAYERS_ITERATIONS 9
+#include "Engine\CommHandler.hpp"
+#include "Engine\Chat.hpp"
+#include "Engine\Taunt.hpp"
+
+#include "Engine\DriveInfo.hpp"
+#include "Engine\DebuggingLog.hpp"
+#include "Engine\Registry.hpp"
+#include "Engine\ManagedArray.hpp"
+
+#include "Engine\Picture.hpp"
+#include "Engine\ClipInfo.hpp"
+
+/* Include panel components: */
+#include "Engine\Panel.hpp"
+#include "Engine\ScrollBarPanel.hpp"
+#include "Engine\TextPanel.hpp"
+#include "Engine\ButtonPanel.hpp"
+#include "Engine\InputPanel.hpp"
+#include "Engine\EditPanel.hpp"
+#include "Engine\ListPanel.hpp"
+#include "Engine\PanelDropDown.hpp"
+#include "Engine\PanelSlider.hpp"
+#include "Engine\PanelEasy.hpp"
+#include "Engine\PanelScreen.hpp"
+#include "Engine\PicturePanel.hpp"
+#include "Engine\MessagePanel.hpp"
+
+#include "Engine\RGE.hpp"
+
+#include "Engine\TRB.hpp"
---
+++ src/Engine/RGE.hpp
@@ -0,0 +1,135 @@
+
+/**
+ * @file    RGE.hpp
+ * @author  Yvan Burrie
+ * @date    2018/06/26
+ * @version 1.0
+ */
+
+/* Include core components: */
+#include "RGE\FileIO.h"
+
+
+/**
+ * This defines the hard-coded number of players.
+ * Many people have been enthusiastic on having more than 9 players.
+ * Modders claim it would be very difficult to do so because the number is hard-coded frequently throughout the build.
+ * Finally, this definition will override the traditional 9 player limit!
+ */
+#define RGE_PLAYERS_COUNT PLAYERS_ITERATIONS
+
+/* Include game components: */
+#include "RGE\BaseGame.hpp"
+#include "RGE\GameInfo.hpp"
+#include "RGE\Campaign.hpp"
+
+/* Include communications components: */
+#include "RGE\CommQueue.hpp"
+#include "RGE\CommLobby.hpp"
+#include "RGE\TimeSinceLastCall.hpp"
+#include "RGE\CommSpeed.hpp"
+#include "RGE\CommSync.hpp"
+#include "RGE\CommAddresses.hpp"
+#include "RGE\CommError.hpp"
+
+/* Include world components: */
+#include "RGE\GameWorld.hpp"
+#include "RGE\ColorTable.hpp"
+#include "RGE\Sound.hpp"
+#include "RGE\Sprite.hpp"
+#include "RGE\Map.hpp"
+#include "RGE\Path.hpp"
+#include "RGE\PathingSystem.hpp"
+#include "RGE\ZoneMap.hpp"
+
+/* Include Random-Map-Module components: */
+#include "RGE\RmmBase.hpp"
+#include "RGE\RmmController.hpp"
+#include "RGE\RmmDbController.hpp"
+#include "RGE\RmmLand.hpp"
+#include "RGE\RmmElevation.hpp"
+#include "RGE\RmmTerrain.hpp"
+#include "RGE\RmmObject.hpp"
+#include "RGE\RmmCliffs.hpp"
+#include "RGE\RmmShallows.hpp"
+
+class RGE_Static_Object;
+class RGE_Animated_Object;
+class RGE_Moving_Object;
+class RGE_Action_Object;
+class RGE_Combat_Object;
+class RGE_Missile_Object;
+
+/* Include Master-Player components: */
+#include "RGE\MasterPlayer.hpp"
+#include "RGE\MasterStaticObject.hpp"
+#include "RGE\MasterAnimatedObject.hpp"
+#include "RGE\MasterDoppleObject.hpp"
+#include "RGE\MasterMovingObject.hpp"
+#include "RGE\MasterActionObject.hpp"
+#include "RGE\TaskList.hpp"
+#include "RGE\Task.hpp"
+#include "RGE\MasterCombatObject.hpp"
+#include "RGE\MasterMissileObject.hpp"
+
+#include "RGE\Player.hpp"
+#include "RGE\VisibleMap.hpp"
+#include "RGE\VictoryConditions.hpp"
+
+/* Include AI-Module components: */
+#include "RGE\InfluenceMap.hpp"
+#include "RGE\AiModule.hpp"
+#include "RGE\AiMainDecisionModule.hpp"
+#include "RGE\AiResourceItem.hpp"
+#include "RGE\AiPlay.hpp"
+#include "RGE\AiBaseItem.hpp"
+#include "RGE\AiBaseObject.hpp"
+#include "RGE\AiBuildItem.hpp"
+#include "RGE\AiBuildModule.hpp"
+#include "RGE\AiUnitModule.hpp"
+#include "RGE\AiConstructionItem.hpp"
+#include "RGE\AiConstructionModule.hpp"
+#include "RGE\AiDiplomacyModule.hpp"
+#include "RGE\AiEmotionalModule.hpp"
+#include "RGE\AiInformationModule.hpp"
+#include "RGE\AiResourceModule.hpp"
+#include "RGE\AiTechnologyItem.hpp"
+#include "RGE\AiResearchModule.hpp"
+#include "RGE\AiStrategyModule.hpp"
+#include "RGE\AiTacticalModule.hpp"
+#include "RGE\AiTradeModule.hpp"
+
+#include "RGE\Effects.hpp"
+
+#include "RGE\Timeline.hpp"
+#include "RGE\Scenario.hpp"
+#include "RGE\Command.hpp"
+
+#include "RGE\ObjectList.hpp"
+#include "RGE\StaticObject.hpp"
+#include "RGE\ActiveSpriteList.hpp"
+#include "RGE\ActiveSprite.hpp"
+#include "RGE\AnimatedObject.hpp"
+#include "RGE\MovingObject.hpp"
+#include "RGE\DopplegangerObject.hpp"
+#include "RGE\DopplegangerCreator.hpp"
+#include "RGE\ActionObject.hpp"
+#include "RGE\ActionList.hpp"
+#include "RGE\Action.hpp"
+#include "RGE\ActionMove.hpp"
+#include "RGE\ActionGather.hpp"
+#include "RGE\ActionExplore.hpp"
+#include "RGE\ActionEnter.hpp"
+#include "RGE\ActionAttack.hpp"
+#include "RGE\ActionBird.hpp"
+#include "RGE\ActionMissile.hpp"
+#include "RGE\ActionTransport.hpp"
+#include "RGE\CombatObject.hpp"
+#include "RGE\MissileObject.hpp"
+
+/* Include Panels: */
+#include "RGE\View.hpp"
+#include "RGE\MainView.hpp"
+#include "RGE\DiamondMap.hpp"
+#include "RGE\DiamondMapView.hpp"
+#include "RGE\PanelToolBox.hpp"
