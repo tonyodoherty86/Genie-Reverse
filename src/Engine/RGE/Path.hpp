ace157437cc350da9f7b4bf547a59330ffd38b8c
Yvan Burrie <yvan.burrie@hotmail.com>
2018-07-01 02:01:33 +1000

    Update Path modules

--- src/Engine/RGE/Path.hpp
+++ src/Engine/RGE/Path.hpp
@@ -2,171 +2,48 @@
 /**
  * @file    Engine\RGE\Path.hpp
  * @author  Yvan Burrie
- * @date    2018/02/20
+ * @date    2018/06/26
  * @version 1.0
  */
 
 struct XYPoint
 {
-    int x;
-    int y;
+    int x, y;
 };
 
 struct XYFloatPoint
 {
-    float x;
-    float y;
+    float x, y;
 };
 
 struct XYZPoint
 {
-    int x;
-    int y;
-    int z;
-};
-
-class PathingSystem
-{
-public:
-
-    struct BYTEPoint
-    {
-        char x;
-        char y;
-    };
-
-    struct FloatHeap
-    {
-        char x;
-        char y;
-        float total;
-    };
-
-    struct MGP_FloatHeap
-    {
-        char x;
-        char y;
-        unsigned __int16 next;
-        int total;
-    };
-
-    int xSizeValue, ySizeValue;
-    char facetValue[255][255];
-    int MGP_costValue[255][255];
-    PathingSystem::MGP_FloatHeap MGP_openPaths[65026];
-    char obstructionValue[1020][255];
-    char miscValue[255][255];
-    int numberOpenPathsValue;
-    PathingSystem::MGP_FloatHeap MGP_bestTraversedPath;
-    int numberTraversedPathsValue;
-
-    class RGE_Map *mapValue;
-    class RGE_Game_World *worldValue;
-    class RGE_Moving_Object *currentObject;
-
-    int currentObjectInObMap,
-        currentTargetID;
-
-    float *currentTerrainTable;
-
-    float currentXOffset,
-          currentYOffset;
-
-    int currentTerrainException1,
-        currentTerrainException2;
-
-    /* TODO */
-    ManagedArray//<int>
-        initialCollidingObjects;
-
-    int startOfPath;
-    int checkTerrainOnFirstPass;
-
-    XYPoint initialTile,
-            minTarget,
-            maxTarget,
-            minInitialPosition,
-            maxInitialPosition;
-
-    float averageNumWaypoints,
-          averagePathLength,
-          averagePathIterations;
-
-    int numAttempts,
-        numFails,
-        numSuccesses,
-        numInitialPathsValue,
-        numContinuePathsValue,
-        numCanPathsValue;
-
-    int MGP_closestTargetDistance;
-
-    float averageClosestTargetDistance,
-          averageSuccessTime,
-          averageFailTime;
-
-    char CurrentFacetMask;
-
-    int currentUnobstructibleGroupID,
-        currentUnobstructiblePlayerID;
-    ManagedArray//<int>
-        currentUnobstructibles;
-
-    int aiPS;
-
-    PathingSystem(int x, int y, int aiPSInit, RGE_Map *map, RGE_Game_World *world);
-
-    ~PathingSystem();
-
-    int initialize(int x, int y, RGE_Map *map, RGE_Game_World *world);
-
-    void printToFile(char *filename);
-
-    char lookupMisc(int x, int y);
-    void setMisc(int x, int y, char v);
-    void initMisc(char v);
-    void zeroObstructionMap();
-    unsigned int obstruction(int x, int y);
-    void incrementObstruction(int x, int y);
-    void decrementObstruction(int x, int y);
-
-    int findTilePath(int startX, int startY, int goalX, int goalY, RGE_Moving_Object *unit, float actionRange, int cTargetID, int copyPathWhenDone, float *pathLength, int checkPathingIterations, int checkTerrainFirstTime, int checkCollisions, int stepSize, int unobstructiblePlayerID, int unobstructibleGroupID);
-
-    void incrementInitialPaths();
-    void incrementContinuePaths();
-    void incrementCanPaths();
-
-    int passable(RGE_Moving_Object *obj, float newX, float newY, int checkTerrain);
-
-    int checksum();
-
-    void printState(RGE_Moving_Object *unit);
-
-    int copyPath(int stepSize);
-    void copyUnobstructibles(ManagedArray/*<int>*/ *d);
+    int x, y, z;
 };
 
 struct XYZBYTEPoint
 {
-    char x;
-    char y;
-    char z;
+    char x, y, z;
 };
 
 struct Waypoint
 {
-    float x;
-    float y;
-    float z;
+    float x, y, z;
+
     char facetToNextWaypoint;
 };
 
-struct Path
+class Path
 {
+public:
+
     unsigned int timeStampValue;
-    Waypoint startValue;
-    Waypoint goalValue;
+
+    Waypoint startValue,
+             goalValue;
+
     Waypoint *pathValue;
+
     int maxSizeValue;
     int currentSizeValue;
     int currentWaypointValue;
@@ -175,9 +52,9 @@ struct Path
 
     ~Path();
 
-    Path *operator+(Path *result/*, Path *__$ReturnUdt*/);
+    Path *operator+(Path *result);
     Path *operator=(Path *p);
-    int operator==(Path *p);
+    bool operator==(Path *p);
 
     int insertAtCurrent(Waypoint *w);
     int insertAtEnd(Waypoint w);
---
+++ src/Engine/RGE/PathingSystem.hpp
@@ -0,0 +1,146 @@
+
+/**
+ * @file    Engine\RGE\PathSystem.hpp
+ * @author  Yvan Burrie
+ * @date    2018/06/26
+ * @version 1.0
+ */
+
+class PathingSystem
+{
+public:
+
+    struct BYTEPoint
+    {
+        char x, y;
+    };
+
+    struct FloatHeap
+    {
+        char x, y;
+
+        float total;
+    };
+
+    struct MGP_FloatHeap
+    {
+        char x, y;
+
+        unsigned short next;
+
+        int total;
+    };
+
+    int xSizeValue, ySizeValue;
+    char facetValue[255][255];
+    int MGP_costValue[255][255];
+
+    PathingSystem::MGP_FloatHeap MGP_openPaths[65026];
+
+    char obstructionValue[1020][255];
+    char miscValue[255][255];
+    int numberOpenPathsValue;
+
+    PathingSystem::MGP_FloatHeap MGP_bestTraversedPath;
+
+    int numberTraversedPathsValue;
+
+    class RGE_Map *mapValue;
+    class RGE_Game_World *worldValue;
+    class RGE_Moving_Object *currentObject;
+
+    int currentObjectInObMap,
+        currentTargetID;
+
+    float *currentTerrainTable;
+
+    float currentXOffset,
+          currentYOffset;
+
+    int currentTerrainException1,
+        currentTerrainException2;
+
+    /* TODO */
+    ManagedArray//<int>
+        initialCollidingObjects;
+
+    int startOfPath;
+    int checkTerrainOnFirstPass;
+
+    XYPoint initialTile,
+            minTarget,
+            maxTarget,
+            minInitialPosition,
+            maxInitialPosition;
+
+    float averageNumWaypoints,
+          averagePathLength,
+          averagePathIterations;
+
+    int numAttempts,
+        numFails,
+        numSuccesses,
+        numInitialPathsValue,
+        numContinuePathsValue,
+        numCanPathsValue;
+
+    int MGP_closestTargetDistance;
+
+    float averageClosestTargetDistance,
+          averageSuccessTime,
+          averageFailTime;
+
+    char CurrentFacetMask;
+
+    int currentUnobstructibleGroupID,
+        currentUnobstructiblePlayerID;
+
+    ManagedArray /*<int>*/ currentUnobstructibles;
+
+    int aiPS;
+
+    PathingSystem(int x, int y, int aiPSInit, RGE_Map *map, RGE_Game_World *world);
+
+    ~PathingSystem();
+
+    int initialize(int x, int y, RGE_Map *map, RGE_Game_World *world);
+
+    void printToFile(char *filename);
+
+    char lookupMisc(int x, int y);
+    void setMisc(int x, int y, char v);
+    void initMisc(char v);
+    void zeroObstructionMap();
+    unsigned int obstruction(int x, int y);
+    void incrementObstruction(int x, int y);
+    void decrementObstruction(int x, int y);
+
+    int findTilePath(int startX,
+                     int startY,
+                     int goalX,
+                     int goalY,
+                     RGE_Moving_Object *unit,
+                     float actionRange,
+                     int cTargetID,
+                     int copyPathWhenDone,
+                     float *pathLength,
+                     int checkPathingIterations,
+                     int checkTerrainFirstTime,
+                     int checkCollisions,
+                     int stepSize,
+                     int unobstructiblePlayerID,
+                     int unobstructibleGroupID);
+
+    void incrementInitialPaths();
+    void incrementContinuePaths();
+    void incrementCanPaths();
+
+    int passable(RGE_Moving_Object *obj, float newX, float newY, int checkTerrain);
+
+    int checksum();
+
+    void printState(RGE_Moving_Object *unit);
+
+    int copyPath(int stepSize);
+    void copyUnobstructibles(ManagedArray/*<int>*/ *d);
+};
