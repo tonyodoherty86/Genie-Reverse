2a90df6a083c7e00b48359cbae25da42c004a71a
Yvan Burrie <yvan.burrie@hotmail.com>
2018-08-23 17:17:01 +1000

    Upload Compression module
    NOTE: This crappy ZLib compression module should be replaced with
    something better and simpler once the Resource-File module has been
    completed.

--- Source/Compress.h
+++ Source/Compress.h
@@ -1,12 +1,19 @@
 
-int deflate_data(void *info_data, void *data, int a3, int a4);
+/**
+ * @file    Compress.hpp
+ * @author  Yvan Burrie
+ * @date    2018/08/21
+ * @version 1.0
+ */
 
-int deflate_deinit(int a1);
+int deflate_data( void *info_data, void *data, int a3, int a4 );
 
-int deflate_init(void *a1, int a2, int a3, int a4, void *a5, int a6, int a7);
+int deflate_deinit( int a1 );
+
+int deflate_init( void *a1, int a2, int a3, int a4, void *a5, int a6, int a7 );
 
 int deflate_buf_size();
 
 int Inf32BufSize();
 
-int Inf32Decode(int Offset, int Size, int Flags);
+int Inf32Decode( int Offset, int Size, int Flags );
