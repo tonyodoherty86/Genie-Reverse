656aea74a60af064c0a3548036761ec215a245cf
Yvan Burrie <yvan.burrie@hotmail.com>
2018-08-23 17:19:00 +1000

    Update Span-List module

--- Source/Engine/SpanList.hpp
+++ Source/Engine/SpanList.hpp
@@ -2,69 +2,15 @@
 /**
  * @file    Engine\SpanList.hpp
  * @author  Yvan Burrie
- * @date    2018/07/01
+ * @date    2018/08/19
  * @version 1.0
  */
-
-class TSpan_Node_List
-{
-public:
-
-    struct VSpan_Node **Zone_Ptrs;
-    int *Zone_Size_Ptrs = 0;
-    int Used_Zones = 0;
-    int Max_Zones = 8;
-
-    struct VSpan_Node *Free_Head;
-    int Free_Pool_Zone = -1;
-    int Free_Pool_Index = -1;
-
-    int Total_Blocks = 0;
-    int Free_Blocks = 0;
-
-    int Default_Grow_Size = 0;
-
-    int Element_Size = 0;
-
-    int Alloc_Count = 0;
-    int Dealloc_Count = 0;
-    int NewZone_Count = 0;
-    int Reset_Count = 0;
-
-    TSpan_Node_List(int InitialSize, int GrowSize, int szitem);
-    ~TSpan_Node_List();
-
-    void InitNewZone(int Zone_Size);
-
-    void SetNumZones(int New_Size);
-
-    VSpan_Node * GetNode();
-
-    void FreeNode(VSpan_Node *theNode);
-
-    int FreeThread(VSpan_Node *HeadNode, VSpan_Node *TailNode);
-
-    void ReclaimAllNodes();
-
-    void ResetStats();
-};
-
-struct VSpan_Node
-{
-    VSpan_Node *Next;
-    VSpan_Node *Prev;
-    int StartPx;
-    int EndPx;
-};
-
-class TShape;
-struct Shape_Info;
 
 class TSpan_List_Manager
 {
 public:
 
-    TSpan_Node_List VSList;
+    TSpan_Node_List *VSList;
 
     VSpan_Node **Line_Head_Ptrs,
                **Line_Tail_Ptrs;
@@ -83,20 +29,43 @@ public:
     int Min_Line,
         Max_Line;
 
-    TSpan_List_Manager(int Surface_Width, int Surface_Height);
+    TSpan_List_Manager( int Surface_Width, int Surface_Height );
+
     ~TSpan_List_Manager();
 
-    void SetSpanRegions(int MinS, int MinL, int MaxS, int MaxL);
+    void SetSpanRegions( int MinS, int MinL, int MaxS, int MaxL );
 
     void ResetAll();
 
-    void AddSpan(int LeftPx, int RightPx, int Line);
+    void AddSpan( int LeftPx, int RightPx, int Line );
 
-    void AddShape(TShape *theShape, int shape_num, int Xpos, int Ypos, int Mirror);
+    void AddShape( TShape *theShape, int shape_num, int Xpos, int Ypos, int Mirror );
 
-    void AddShape_Align(char *Shape_Base, Shape_Info *SI, int Xpos, int Ypos, int Mirror);
+    void AddShape_Align( char *Shape_Base, Shape_Info *SI, int Xpos, int Ypos, int Mirror );
 
-    void AddLine_Align(int x1, int y1, int x2, int y2);
+    void AddLine_Align( int x1, int y1, int x2, int y2 );
+
+    void AddMiniList( VSpanMiniList *theList, int Xpos, int Ypos );
+    void SubtractMiniList( VSpanMiniList *theList, int Xpos, int Ypos );
+
+    void DeleteSpan( int LeftPx, int RightPx, int Line );
+    void DeleteShape( TShape *theShape, int shape_num, int Xpos, int Ypos, int Mirror );
+    VSpan_Node *PointVisible( int x, int Line );
+    void Merge_n_Align( TSpan_List_Manager *List1, TSpan_List_Manager *List2 );
+    void AlignamizeSpans();
+    void ScrollSpansHorizontally( int XOffset, int ClipToCurrentRegion );
+    void ScrollSpansVertically( int YOffset, int ClipToCurrentRegion );
+    int ValidateSpan( int Line );
+    int DecodeLine( char *LineBuf, int Line, char fillC, int MaxX );
+
+    void take_snapshot(
+        char *filespec,
+        int *index_no,
+        TDrawArea *render_area,
+        int scolor,
+        TSpan_List_Manager **SpanArray,
+        int *colorArray,
+        int ArraySize );
 };
 
 struct VSpanMiniList
@@ -104,4 +73,57 @@ struct VSpanMiniList
     char Y_delta,
          X_start,
          X_end;
+};
+
+class TSpan_Node_List
+{
+    public:
+
+    VSpan_Node **Zone_Ptrs;
+
+    int *Zone_Size_Ptrs;
+
+    unsigned int Used_Zones,
+                 Max_Zones = 8;
+
+    VSpan_Node *Free_Head;
+
+    int Free_Pool_Zone  = -1,
+        Free_Pool_Index = -1;
+
+    int Total_Blocks,
+        Free_Blocks;
+
+    int Default_Grow_Size,
+        Element_Size;
+
+    int Alloc_Count,
+        Dealloc_Count,
+        NewZone_Count,
+        Reset_Count;
+
+    public: TSpan_Node_List( unsigned int InitialSize, unsigned int GrowSize, size_t szitem );
+
+    public: ~TSpan_Node_List();
+
+    private: void InitNewZone( unsigned int Zone_Size );
+
+    private: void SetNumZones( size_t New_Size );
+
+    public: VSpan_Node *GetNode();
+
+    public: void FreeNode( VSpan_Node *theNode );
+
+    public: int FreeThread( VSpan_Node *HeadNode, VSpan_Node *TailNode );
+
+    public: void ReclaimAllNodes();
+
+    public: void ResetStats();
+};
+
+struct VSpan_Node
+{
+    VSpan_Node *Next, *Prev;
+
+    int StartPx, EndPx;
 };
